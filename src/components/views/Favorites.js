d84b3ed1184c5c9d41932f8d723fb2176ef909b9
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-01-13 20:01:41 +0100

    gh pages configuracion

--- src/components/views/Favorites.js
+++ src/components/views/Favorites.js
@@ -14,6 +14,7 @@ const Favoritos = function () {
   const handleSetFavs = (e)=> {setListFavs(e)}
 
   useEffect(() => {
+    console.log(personajes)
     listarFavoritos(setPersonajes, listFav);
 
   }, [ listFav])
