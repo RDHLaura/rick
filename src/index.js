3d6e52256206c3edf16357aa5ada557ca112c9ef
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-01-13 16:36:09 +0100

    configuración gh pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/sass/main.sass'
 import App from './App';
-import 'https://kit.fontawesome.com/26a47be34a.js'
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
