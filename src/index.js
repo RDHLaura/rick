0486bd3164b6ba930770a69e5896329463f44908
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-01-13 20:19:50 +0100

    gh pages configuracion

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <App />
+
+      <App />
+
   </React.StrictMode>
 );
 
